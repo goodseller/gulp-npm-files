2855c341f529f7304a3adb3440d7a27e7eb36a03
Nipher <github@nipher.com.br>
2015-03-19 16:04:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+module.exports = function(devDependencies) {
+  var buffer, packages, keys;
+  
+  buffer = fs.readFileSync('./package.json');
+  packages = JSON.parse(buffer.toString());
+  keys = [];
+  
+  for (key in packages.dependencies) {
+    keys.push('./node_modules/' + key + '/**/*');
+  }
+
+  if (devDependencies) {
+    for (key in packages.devDependencies) {
+      keys.push('./node_modules/' + key + '/**/*');
+    }
+  }
+
+  return keys;
+};
